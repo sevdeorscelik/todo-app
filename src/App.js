c662909a5dc8582e0f84bb37708f489de8bfaca8
Sevde <sevdeorscelik@gmail.com>
2022-07-21 03:26:18 +0200

    finished

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Todo from './components/todo';
+
+function App() {
+  return (
+    <div className='App container mt-5 d-flex flex-column justify-content-center align-items-center '>
+      
+      <Todo/>
+
+    </div>
+  );
+}
+
+export default App;
